b8cafc0f92a29c16c079138aacac18df0906f049
John Chodera <john.chodera@choderalab.org>
2018-11-04 09:51:43 +0100

    Refine CUDA install

--- devtools/appveyor/install_cuda.cmd
+++ devtools/appveyor/install_cuda.cmd
@@ -1,28 +1,36 @@
 @echo off
 echo Downloading CUDA toolkit 9.2
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers2/cuda_9.2.148_windows -FileName cuda.exe
-echo Installing CUDA toolkit
-cuda.exe -s compiler_9.2 ^
-                           cublas_9.2 ^
-                           cublas_dev_9.2 ^
-                           cudart_9.2 ^
-                           curand_9.2 ^
-                           curand_dev_9.2
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_411.31_windows -FileName cuda_10.0.130_411.31_windows.exe
 
+dir "%ProgramFiles%"
+dir "C:\Program Files"
+
+echo Installing CUDA toolkit
+cuda_10.0.130_411.31_windows.exe -s nvcc_10.0 ^
+            memcheck_10.0 ^
+            cublas_10.0 ^
+            cublas_dev_10.0 ^
+            cudart_10.0 ^
+            cufft_dev_10.0 ^
+            curand_10.0 ^
+            curand_dev_10.0 ^
+            nvrtc_10.0 ^
+            nvrtc_dev_10.0
+            
 dir .
 
 dir "%ProgramFiles%"
 dir "C:\Program Files"
 dir "C:\Program Files\NVIDIA Corporation"
-dir "C:\Program Files\NVIDIA Corporation\NVIDIA GPU Computing Toolkit\CUDA"
-dir "C:\Program Files\NVIDIA Corporation\NVIDIA GPU Computing Toolkit\CUDA\v9.2"
-dir "C:\Program Files\NVIDIA Corporation\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.0"
 
-if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin\cudart64_80.dll" (
+if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin\cudart64_80.dll" (
 echo "Failed to install CUDA"
 exit /B 1
 )
 
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v10.0\libnvvp;%PATH%
 
 nvcc -V
