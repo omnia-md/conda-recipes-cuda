c63a089f540883ba0f547d42da476620ee4749b7
John Chodera <john.chodera@choderalab.org>
2018-11-06 10:41:15 +0200

    Install and build OpenMM for CUDA 10.0

--- openmm/bld.bat
+++ openmm/bld.bat
@@ -2,10 +2,9 @@ mkdir build
 cd build
 
 set FFTW=C:\Miniconda3\pkgs\fftw3f-3.3.4-vc14_2\Library
-set APPSDK=C:\Program Files (x86)\AMD APP SDK\2.9-1
-set CUDASDK=C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v9.2
+set CUDASDK=C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.0
 "C:\Program Files\CMake\bin\cmake.exe" .. -G "NMake Makefiles JOM" -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_BUILD_TYPE=Release -DOPENMM_GENERATE_API_DOCS=ON ^
-    -DOPENCL_INCLUDE_DIR="%APPSDK%\include" -DOPENCL_LIBRARY="%APPSDK%\lib\x86_64\OpenCL.lib" ^
+    -DOPENCL_INCLUDE_DIR="%CUDASDK%\include" -DOPENCL_LIBRARY="%CUDASDK%\lib\x64\OpenCL.lib" ^
     -DFFTW_INCLUDES="%FFTW%/include" -DFFTW_LIBRARY="%FFTW%/lib/libfftw3f-3.lib" -DCUDA_TOOLKIT_ROOT_DIR="%CUDASDK%"
 
 jom install
