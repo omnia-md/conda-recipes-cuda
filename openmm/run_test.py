619f56bfb09e2478a982bae4ac473966f82c98f2
John Chodera <john.chodera@choderalab.org>
2019-07-08 01:08:46 -0400

    Correct git revision in tests.

--- openmm/run_test.py
+++ openmm/run_test.py
@@ -7,4 +7,4 @@ from simtk import openmm
 assert openmm.Platform.getOpenMMVersion() == '7.4', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
-assert openmm.version.git_revision == '4a269c0d64f995c00b9a6949e5c7839c536c1837', "openmm.version.git_revision = %s" % openmm.version.git_revision
+assert openmm.version.git_revision == 'b9a1bee6da1ffbcc2fd8300bc710c4166f30cbf8', "openmm.version.git_revision = %s" % openmm.version.git_revision
