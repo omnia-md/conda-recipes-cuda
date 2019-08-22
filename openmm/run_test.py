5dedfa055677917fbaa849fb16c39995a3162789
John Chodera <john.chodera@choderalab.org>
2019-08-22 01:36:13 -0400

    7.4.0 rc1

--- openmm/run_test.py
+++ openmm/run_test.py
@@ -7,4 +7,4 @@ from simtk import openmm
 assert openmm.Platform.getOpenMMVersion() == '7.4', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
-assert openmm.version.git_revision == 'b9a1bee6da1ffbcc2fd8300bc710c4166f30cbf8', "openmm.version.git_revision = %s" % openmm.version.git_revision
+assert openmm.version.git_revision == 'b71e92d9ccef7c98cfd6b862eb34095d94fa1b05', "openmm.version.git_revision = %s" % openmm.version.git_revision
