fc788a60a5a0e3e3e64e5029e44539bb85dfe2d7
peastman <peastman@stanford.edu>
2019-08-27 13:16:21 -0700

    Fixed conda version

--- devtools/osx-build.sh
+++ devtools/osx-build.sh
@@ -14,7 +14,7 @@ export MACOSX_DEPLOYMENT_TARGET="10.13"
 #brew update-reset
 
 # Install Miniconda
-curl -s -O https://repo.continuum.io/miniconda/Miniconda2-4.6.14-MacOSX-x86_64.sh;
+curl -s -O https://repo.continuum.io/miniconda/Miniconda3-4.6.14-MacOSX-x86_64.sh;
 bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $HOME/anaconda;
 export PATH=$HOME/anaconda/bin:$PATH;
 conda config --add channels conda-forge;
