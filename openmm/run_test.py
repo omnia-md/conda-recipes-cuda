9579229052fd643df7de75197877e4e74699244e
peastman <peastman@stanford.edu>
2019-11-19 12:13:03 -0800

    OpenMM 7.4.1 rc1

--- openmm/run_test.py
+++ openmm/run_test.py
@@ -4,7 +4,7 @@ from simtk import openmm
 
 # Check major version number
 # If Z=0 for version X.Y.Z, out put is "X.Y"
-assert openmm.Platform.getOpenMMVersion() == '7.4', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
+assert openmm.Platform.getOpenMMVersion() == '7.4.1', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
-assert openmm.version.git_revision == 'b71e92d9ccef7c98cfd6b862eb34095d94fa1b05', "openmm.version.git_revision = %s" % openmm.version.git_revision
+assert openmm.version.git_revision == '989a9912c8281f3f85d1ae89ef492f9a65c70fab', "openmm.version.git_revision = %s" % openmm.version.git_revision
